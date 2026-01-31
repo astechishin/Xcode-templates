c089e499603ffa7914e92229a5c13991df10d7eb
Andy Stechishin <andy.stechishin@gmail.com>
2026-01-31 16:42:33 -0700

    Add new methods for random string generation to TestSetup

--- file-templates/Test/Swift Testing Setup.xctemplate/___FILEBASENAME___.swift
+++ file-templates/Test/Swift Testing Setup.xctemplate/___FILEBASENAME___.swift
@@ -13,6 +13,37 @@ class ___FILEBASENAMEASIDENTIFIER___ {
                             configurations: ModelConfiguration(isStoredInMemoryOnly: true))
     }
 
+    enum StringContent {
+        case lettersDigits
+        case lettersDigitsSymbols
+        case lettersDigitsSymbolsNewlines
+    }
+
+    static func randomString(length: Int = 12, charTypes: StringContent = .lettersDigits) -> String {
+        let letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+        let symbols = "!@#$%^&*(){}[]\\|/?<>~-+=:;.,"
+        let newline = "\n"
+        let characterSet: String
+
+        switch charTypes {
+        case .lettersDigits: characterSet = letters
+        case .lettersDigitsSymbols: characterSet = letters + symbols
+        case .lettersDigitsSymbolsNewlines: characterSet = letters + symbols + newline
+        }
+
+        var randomString = ""
+        for _ in 0 ..< length {
+            let randomIndex = Int(arc4random_uniform(UInt32(characterSet.count)))
+            let letter = characterSet[characterSet.index(characterSet.startIndex, offsetBy: randomIndex)]
+            randomString += String(letter)
+        }
+        return randomString
+    }
+
+    static func randomLengthRandomString(_ range: Range<Int>) -> String {
+        return randomString(length: Int.random(in: range))
+    }
+
     static func validateLoadFile(_ fileName: String, bundle: Bundle? = nil, file: StaticString=#filePath, line: UInt=#line) throws -> URL {
         let fileBundle = bundle ?? Self.bundle
         let urlIsh = fileBundle.url(forResource: fileName, withExtension: "")
